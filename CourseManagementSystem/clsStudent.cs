2177249cd4a9d46a849d1df3dbad9989cec21626
Heavenly Chour <heavchour@gmail.com>
2017-08-28 19:23:03 +1000

    clsStudent constructors made. Getting strange formatting for getters and setters with refactoring in my Visual Studio so I left those out.

--- CourseManagementSystem/clsStudent.cs
+++ CourseManagementSystem/clsStudent.cs
@@ -9,7 +9,49 @@ namespace CourseManagementSystem
     public class clsStudent
     {
         private int studentID;
+        private string firstName;
+        private string lastName;
+        private string dateOfBirth;
+        private string gender;
+        private string streetAddress;
+        private string suburb;
+        private int postCode;
+        private int phoneNumber;
+        private string email;
+        private string nationality;
+        private bool disability;
 
+        public clsStudent()
+        {
+        }
 
+        public clsStudent(int studentID)
+        {
+            this.studentID = studentID;
+        }
+
+        public clsStudent(int studentID, string firstName, string lastName)
+        {
+            this.studentID = studentID;
+            this.firstName = firstName;
+            this.lastName = lastName;
+        }
+
+        public clsStudent(int studentID, string firstName, string lastName, string dateOfBirth, string gender, string streetAddress, string suburb, int postCode, int phoneNumber, string email, string nationality, bool disability)
+        {
+            this.studentID = studentID;
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.dateOfBirth = dateOfBirth;
+            this.gender = gender;
+            this.streetAddress = streetAddress;
+            this.suburb = suburb;
+            this.postCode = postCode;
+            this.phoneNumber = phoneNumber;
+            this.email = email;
+            this.nationality = nationality;
+            this.disability = disability;
+        }
+        
     }
 }
